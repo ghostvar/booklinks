2d505c77c0ea4b7e5ae30edab40cf0e10f0453d7
Bagus Fahrudin <bagus_fahrudin_05@hotmail.com>
2018-01-14 12:56:15 +0700

    Template dasar + remove production code

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,5 +1,45 @@
 <template>
-    <example-component></example-component>
+<div>
+    <section class="hero is-primary hero-header">
+        <div class="hero-body">
+            <div class="container-fluid">
+                <h1 class="title">Primary title</h1>
+                <h2 class="subtitle">Primary subtitle</h2>
+            </div>
+        </div>
+    </section>
+    <div class="columns">
+      <!-- MENU -->
+      <div class="column is-2 column-menu">
+        <aside class="menu hero is-fullheight is-info">
+          <ul class="menu-list">
+            <li>
+              <a><i class="fa fa-file-text-o"></i>Formulir</a>
+            </li>
+            <li>
+              <a><i class="fa fa-building-o"></i>Pesantren</a>
+            </li>
+            <li>
+              <a><i class="fa fa-university"></i>Lembaga</a>
+            </li>
+            <li>
+              <a><i class="fa fa-level-up"></i>Alumni</a>
+            </li>
+            <li>
+              <a><i class="fa fa-cogs"></i>Pengaturan</a>
+            </li>
+            <li>
+              <a class="is-active"><i class="fa fa-bar-chart"></i>Statistik</a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+      <!-- KONTEN -->
+      <div class="column">
+        
+      </div>
+    </div>
+</div>    
 </template>
 
 <script>
