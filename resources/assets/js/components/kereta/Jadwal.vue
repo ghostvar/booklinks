8cd3d30173f128e87d9f9057cf7f0eae0923a0c9
Bagus Fahrudin <bagus_fahrudin_05@hotmail.com>
2018-02-01 13:06:44 +0700

    Form rute & jadwal awal

--- resources/assets/js/components/kereta/Jadwal.vue
+++ resources/assets/js/components/kereta/Jadwal.vue
@@ -1,44 +1,127 @@
 <template>
   <div>
-    <div class="control is-nested">
-      <span v-if="!used.id">
-        <button class="button is-success">Tambah</button>
-      </span>
-      <span v-if="used.id">
-        <button class="button is-primary" @click="initJadwalKereta">Batal</button>
-        <button class="button is-success">Ubah</button>
-        <button class="button is-danger">Hapus</button>
-      </span>
-    </div>
-    <table class="table is-fullwidth is-hoverable is-small">
-      <thead>
-        <tr>
-          <th>No</th>
-          <th>Nama Kereta</th>
-          <th>Waktu Keberangkatan</th>
-          <th>Stasiun Keberangkatan</th>
-          <th>Waktu Kedatangan</th>
-          <th>Stasiun Kedatangan</th>
-          <th>Waktu Tempuh</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(jadwal, id) in $store.state.jadwalKereta" :key="id" @click="selectJadwalKereta(jadwal)" :class="{ 'is-selected': (used.id == jadwal.id ? true:false) }">
-          <td v-text="id+1"></td>
-          <td v-text="jadwal.kereta.name"></td>
-          <td v-text="jadwal.waktu_berangkat"></td>
-          <td v-text="jadwal.stasiun_berangkat"></td>
-          <td v-text="jadwal.waktu_sampai"></td>
-          <td v-text="jadwal.stasiun_sampai"></td>
-          <td v-text="jadwal.waktu_tempuh + ' Jam'"></td>
-        </tr>
-      </tbody>
-    </table>
-  </div>    
+    <span v-if="viewMode == 'default'">
+      <div class="control is-nested">
+        <span v-if="!used.id">
+          <button class="button is-success" @click="viewMode = 'add'">Tambah</button>
+        </span>
+        <span v-if="used.id">
+          <button class="button is-light is-text" @click="initJadwalKereta">Batal</button>
+          <button class="button is-success">Ubah</button>
+          <button class="button is-danger">Hapus</button>
+        </span>
+      </div>
+      <table class="table is-fullwidth is-hoverable is-small">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Nama Kereta</th>
+            <th>Waktu Keberangkatan</th>
+            <th>Stasiun Keberangkatan</th>
+            <th>Waktu Kedatangan</th>
+            <th>Stasiun Kedatangan</th>
+            <th>Waktu Tempuh</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(jadwal, id) in $store.state.jadwalKereta" :key="id" @click="selectJadwalKereta(jadwal)" :class="{ 'is-selected': (used.id == jadwal.id ? true:false) }">
+            <td v-text="id+1"></td>
+            <td v-text="jadwal.kereta.name"></td>
+            <td v-text="jadwal.rute[0].waktu_berangkat"></td>
+            <td v-text="jadwal.rute[0].stasiun_berangkat.name"></td>
+            <td v-text="jadwal.rute[jadwal.rute.length-1].waktu_sampai"></td>
+            <td v-text="jadwal.rute[jadwal.rute.length-1].stasiun_sampai.name"></td>
+            <td v-text="jadwal.waktu_tempuh + ' Jam'"></td>
+          </tr>
+        </tbody>
+      </table>
+    </span>
+    <span v-if="viewMode == 'add'">
+      <div class="control is-nested">
+        <button class="button is-light is-text" @click="viewMode = 'default'">Kembali</button>
+      </div>
+      <div class="columns">
+        <div class="column is-4">
+          <div class="field">
+            <label class="label">Kereta</label>
+            <div class="control">
+              <div class="select is-small">
+                <select>
+                  <option>Pilih Kereta</option>
+                  <option v-for="(kereta, id) in $store.state.kereta" :key="id" v-text="kereta.name"></option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-6">
+          <div class="field">
+            <label class="label">Keberangkatan</label>
+            <div class="control">
+              <div class="select is-small">
+                <select>
+                  <option>Pilih Stasiun</option>
+                  <option v-for="(stasiun, id) in $store.state.stasiun" :key="id" :value="stasiun.kode" v-text="stasiun.name"></option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="time" placeholder="">
+            </div>
+          </div>
+        </div>
+        <div class="column is-6">
+          <div class="field">
+            <label class="label">Kedatangan</label>
+            <div class="control">
+              <div class="select is-small">
+                <select>
+                  <option>Pilih Stasiun</option>
+                  <option v-for="(stasiun, id) in $store.state.stasiun" :key="id" :value="stasiun.kode" v-text="stasiun.name"></option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="field">
+            <div class="control">
+              <input class="input is-small" type="time" placeholder="">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <table class="table is-fullwidth is-hoverable is-small">
+          <thead>
+            <tr>
+              <th>No</th>
+              <th>Stasiun Keberangkatan</th>
+              <th>Waktu Berangkat</th>
+              <th>Stasiun Kedatangan</th>
+              <th>Waktu Sampai</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </span>
+  </div> 
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      viewMode: 'default'
+    }
+  },
   mounted() {
     this.initJadwalKereta();
   }
