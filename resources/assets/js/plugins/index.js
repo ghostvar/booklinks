eea5dd5468c5393645701f2e0751f8a8e3179c3c
Bagus Fahrudin <bagus_fahrudin_05@hotmail.com>
2018-02-12 09:34:24 +0700

    example pemesanan data

--- resources/assets/js/plugins/index.js
+++ resources/assets/js/plugins/index.js
@@ -1,4 +1,3 @@
-import data from './data.json'
 import Pelanggan from './models/Pelanggan'
 import Kereta from './models/Kereta'
 import Penerbangan from './models/Penerbangan'
@@ -32,7 +31,11 @@ export default {
                         this.getTime();
                     }, 1000)
 
-                    this.$store.commit('setPemesanan', data.pemesanan);
+                    this.$store.commit('setPemesanan', 
+                        { "id": "0", "name": "Rian", "booking_code": "ASM10293", "jenis": "Penerbangan", "created_at": "7 Mei 2018", "updated_at": "8 April 2017", "status": "", "status_pembayaran": "" },
+                        { "id": "1", "name": "Fadli", "booking_code": "AAM102293", "jenis": "Kereta", "created_at": "14 April 2018", "updated_at": "2 Juni 2017", "status": "", "status_pembayaran": "" },
+                        { "id": "2", "name": "Ahmad", "booking_code": "ASM10232", "jenis": "Kereta", "created_at": "02 Februari 2018", "updated_at": "28 November 2017", "status": "", "status_pembayaran": "" }
+                    );
                     Pelanggan.getCostumers(data => { this.$store.commit('setPelanggan', data) });
                     Kereta.getStasiun(data => { this.$store.commit('setStasiun', data); });
                     Kereta.getTrains(data => { this.$store.commit('setKereta', data); });
